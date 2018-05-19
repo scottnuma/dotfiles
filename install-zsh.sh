feef465298af4ee614e1f1f595f3188e8891ce13
scottnuma <scott.numamoto@gmail.com>
2018-05-19 15:53:59 -0700

    Add zsh installation

---
+++ install-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install zsh
+sudo apt-get install zsh
+
+# Install Oh My Zsh - https://github.com/robbyrussell/oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
