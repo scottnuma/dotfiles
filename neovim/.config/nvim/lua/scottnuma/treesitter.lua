3ed4e7b80614e3599ddf669f0e7a0f1f93b74a0b
scottnuma <scott.numamoto@gmail.com>
2021-04-04 23:29:14 -0700

    Simplify indentation

--- neovim/.config/nvim/lua/scottnuma/treesitter.lua
+++ neovim/.config/nvim/lua/scottnuma/treesitter.lua
@@ -14,8 +14,5 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,              -- false will disable the whole extension
   },
-  indent = {
-    enable = true
-  }
 }
 
