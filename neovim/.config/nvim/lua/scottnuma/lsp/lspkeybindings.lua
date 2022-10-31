c93222a327cf9e1c998d109ca172883cc393b42d
scottnuma <scott.numamoto@gmail.com>
2022-10-31 10:37:59 -0700

    Move off deprecated keybind

--- neovim/.config/nvim/lua/scottnuma/lsp/lspkeybindings.lua
+++ neovim/.config/nvim/lua/scottnuma/lsp/lspkeybindings.lua
@@ -54,11 +54,11 @@ on_attach = function(client, bufnr)
         -- Sets the location list
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
         -- Formats the current buffer.
 
-  elseif client.resolved_capabilities.document_range_formatting then
+  elseif client.server_capabilities.document_range_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
         -- See :help
   end
